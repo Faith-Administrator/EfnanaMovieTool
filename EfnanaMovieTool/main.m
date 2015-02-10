f950d3fd5515cf5416491a53da6446d76fcc9689
j_maruyama <j_maruyama@faith.co.jp>
2015-02-10 15:34:14 +0900

    Initial Commit

---
+++ EfnanaMovieTool/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  EfnanaMovieTool
+//
+//  Created by j_maruyama on 2015/02/10.
+//  Copyright (c) 2015年 j_maruyama. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
